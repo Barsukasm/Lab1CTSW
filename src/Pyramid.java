e042a6cf1712ba2f286e95f6e4b7fdc67b05f411
Barsukasm <nb721@mail.ru>
2019-12-03 20:45:56 +0700

    added declaration of methods

--- src/Pyramid.java
+++ src/Pyramid.java
@@ -1,2 +1,37 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class Pyramid <T> {
+    private ArrayList<T> values;
+    private ArrayList<Integer> emptyInd;
+
+    Pyramid(){
+        emptyInd = new ArrayList<>();
+        values = new ArrayList<>();
+    }
+
+    public void insert(T elem){}
+
+    public T findByIndex(int index){
+        T item = null;
+        return item;
+    }
+
+    public void removeByIndex(int index){}
+
+    public class PyramidIterator implements Iterator{
+
+        public T next(){
+            T item = null;
+            return item;
+        }
+
+        public boolean hasNext(){
+            return true;
+        }
+
+        public void forEach(){
+
+        }
+    }
 }
