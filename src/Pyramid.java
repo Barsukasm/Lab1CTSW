29ed6b45696430f94a8237f673c4280e3c05175a
Nikolay Barsukov <nb721@mail.ru>
2019-12-04 12:24:02 +0700

    remake all

--- src/Pyramid.java
+++ src/Pyramid.java
@@ -1,21 +1,31 @@
+import java.util.AbstractList;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Iterator;
 
-public class Pyramid <T> {
-    private ArrayList<T> values;
-    private ArrayList<Integer> emptyInd;
+public class Pyramid <T>{
+    private Object[] values;
+    private int[] emptyIndexes;
 
     Pyramid(){
-        emptyInd = new ArrayList<>();
-        values = new ArrayList<>();
+        this.values = new Object[10];
+        this.emptyIndexes = new int[10];
+        Arrays.fill(emptyIndexes,0);
     }
 
-    public void insert(T elem){}
+    public void grow(){
+        this.values = Arrays.copyOf(this.values, this.values.length*2);
+    }
+    public void insert(T elem){
 
-    public T findByIndex(int index){
+    }
+
+    public T get(int index){
         T item = null;
         return item;
     }
+
+    public int size(){ return this.values.length; }
 
     public void removeByIndex(int index){}
 
